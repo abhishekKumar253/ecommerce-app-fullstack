d54f6c30fec6ba38790059c4a623efd8f5fbe09b
abhishekkumar4553 <abhishekgupta45591@gmail.com>
2025-01-20 21:33:54 +0530

    project done

--- backend/src/server.js
+++ backend/src/server.js
@@ -2,19 +2,8 @@ import { config } from "./db/config.js";
 import { app } from "./app.js";
 import dotenv from "dotenv";
 import connectDB from "./db/index.js";
-import path from "path";
 
 dotenv.config();
-
-const __dirname = path.resolve();
-
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "frontend/dist")));
-
-  app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
-  });
-}
 
 connectDB()
   .then(() => {
