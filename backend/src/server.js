85ad7f4b67edba9f371410ad8efd5b5ecd5dd88b
abhishekkumar4553 <abhishekgupta45591@gmail.com>
2025-02-28 17:02:38 +0530

    feat(server): serve static files in production and update environment variables

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,14 +1,26 @@
+import express from "express";
 import { config } from "./db/config.js";
 import { app } from "./app.js";
 import dotenv from "dotenv";
 import connectDB from "./db/index.js";
+import path from "path";
 
 dotenv.config();
 
+const port = process.env.PORT || config.port;
+
 connectDB()
   .then(() => {
-    app.listen(config.port, () => {
-      console.log(`⚙️ Server is running on port ${config.port}`);
+    if (process.env.NODE_ENV === "production") {
+      app.use(express.static(path.join(__dirname, "frontend", "dist")));
+
+      app.get("*", (req, res) => {
+        res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"));
+      });
+    }
+
+    app.listen(port, () => {
+      console.log(`⚙️ Server is running on port ${port}`);
     });
   })
   .catch((err) => {
