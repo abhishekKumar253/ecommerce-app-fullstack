4aa0222f04816b7e3f3de52a0f6c55adf738c5f2
abhishekkumar4553 <abhishekgupta45591@gmail.com>
2026-03-26 00:58:58 +0530

    navbar.jsx

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -14,25 +14,22 @@ const Navbar = () => {
         <div className="flex flex-wrap justify-between items-center">
           <Link
             to="/"
-            className="text-2xl font-bold text-emerald-400 items-center space-x-2 flex"
-          >
-            E-Commerce
+            className="text-2xl font-bold text-emerald-400 items-center space-x-2 flex">
+            E-Shop Mart
           </Link>
 
           <nav className="flex flex-wrap items-center gap-4">
             <Link
               to={"/"}
               className="text-gray-300 hover:text-emerald-400 transition duration-300
-					 ease-in-out"
-            >
+					 ease-in-out">
               Home
             </Link>
             {user && (
               <Link
                 to={"/cart"}
                 className="relative group text-gray-300 hover:text-emerald-400 transition duration-300 
-							ease-in-out"
-              >
+							ease-in-out">
                 <ShoppingCart
                   className="inline-block mr-1 group-hover:text-emerald-400"
                   size={20}
@@ -41,8 +38,7 @@ const Navbar = () => {
                 {cart.length > 0 && (
                   <span
                     className="absolute -top-2 -left-2 bg-emerald-500 text-white rounded-full px-2 py-0.5 
-									text-xs group-hover:bg-emerald-400 transition duration-300 ease-in-out"
-                  >
+									text-xs group-hover:bg-emerald-400 transition duration-300 ease-in-out">
                     {cart.length}
                   </span>
                 )}
@@ -52,8 +48,7 @@ const Navbar = () => {
               <Link
                 className="bg-emerald-700 hover:bg-emerald-600 text-white px-3 py-1 rounded-md font-medium
 								 transition duration-300 ease-in-out flex items-center"
-                to={"/secret-dashboard"}
-              >
+                to={"/secret-dashboard"}>
                 <Lock className="inline-block mr-1" size={18} />
                 <span className="hidden sm:inline">Dashboard</span>
               </Link>
@@ -63,8 +58,7 @@ const Navbar = () => {
               <button
                 className="bg-gray-700 hover:bg-gray-600 text-white py-2 px-4 
 						rounded-md flex items-center transition duration-300 ease-in-out"
-                onClick={logout}
-              >
+                onClick={logout}>
                 <LogOut size={18} />
                 <span className="hidden sm:inline ml-2">Log Out</span>
               </button>
@@ -73,16 +67,14 @@ const Navbar = () => {
                 <Link
                   to={"/signup"}
                   className="bg-emerald-600 hover:bg-emerald-700 text-white py-2 px-4 
-									rounded-md flex items-center transition duration-300 ease-in-out"
-                >
+									rounded-md flex items-center transition duration-300 ease-in-out">
                   <UserPlus className="mr-2" size={18} />
                   Sign Up
                 </Link>
                 <Link
                   to={"/login"}
                   className="bg-gray-700 hover:bg-gray-600 text-white py-2 px-4 
-									rounded-md flex items-center transition duration-300 ease-in-out"
-                >
+									rounded-md flex items-center transition duration-300 ease-in-out">
                   <LogIn className="mr-2" size={18} />
                   Login
                 </Link>
